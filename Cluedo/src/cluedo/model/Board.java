33d9d70157228d54de5cc3361773899067e22306
kudos3939 <annie1020k@gmail.com>
2014-08-11 21:06:40 +1200

    added basic constructor and repain() to BoardFrame

--- Cluedo/src/cluedo/model/Board.java
+++ Cluedo/src/cluedo/model/Board.java
@@ -1,5 +1,9 @@
 package cluedo.model;
 
 public class Board {
+    Card solution;
+    Room rooms;
+    Character characters;
+    Weapon weapons;
 
 }
--- Cluedo/src/cluedo/view/BoardFrame.java
+++ Cluedo/src/cluedo/view/BoardFrame.java
@@ -1,7 +1,37 @@
 package cluedo.view;
 
+import cluedo.model.*;
+
 import javax.swing.JFrame;
+import java.awt.*;
+import java.awt.event.MouseListener;
 
 public class BoardFrame extends JFrame {
+    private final BoardCanvas canvas;
+
+    public BoardFrame(String title, Board game, MouseListener mouse) {
+        super(title);
+
+        canvas = new BoardCanvas(/* uid,game */);
+        //it needs just one Listener?
+        canvas.addMouseListener(mouse);
+
+        //does this line necessary?
+        setLayout(new BorderLayout());
+        add(canvas, BorderLayout.CENTER);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //pack components nicely
+        pack();
+        //we don't want user to change window size
+        setResizable(false);
+
+        //set UI visible
+        setVisible(true);
+    }
+
+    public void repaint() {
+        canvas.repaint();
+    }
 
 }
