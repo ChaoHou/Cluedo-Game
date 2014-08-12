0b89b11f341ac353f9363eecc174a247eeacd5f2
kudos3939 <annie1020k@gmail.com>
2014-08-13 04:31:04 +1200

    add basic constructor and getName()

--- Cluedo/src/cluedo/model/Room.java
+++ Cluedo/src/cluedo/model/Room.java
@@ -1,5 +1,17 @@
 package cluedo.model;
 
 public class Room {
+    private final String name;
 
+    public Room(String name) {
+        this.name = name;
+    }
+
+    /**
+     * get method for the name;
+     * @return String
+     */
+    public String getName() {
+        return name;
+    }
 }
--- Cluedo/src/cluedo/model/Weapon.java
+++ Cluedo/src/cluedo/model/Weapon.java
@@ -1,5 +1,18 @@
 package cluedo.model;
 
 public class Weapon {
+    private final String name;
+
+    public Weapon(String name) {
+        this.name = name;
+    }
+
+    /**
+     * get method for the name
+     * @return String
+     */
+    public String getName() {
+        return name;
+    }
 
 }
