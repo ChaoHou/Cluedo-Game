f07e311499441382b8e6452d58eb82bdf2ac31e6
kudos3939 <annie1020k@gmail.com>
2014-08-13 04:30:17 +1200

    add enum TYPE, ROOM, WEAPON and CHARACTER

--- Cluedo/src/cluedo/model/Card.java
+++ Cluedo/src/cluedo/model/Card.java
@@ -1,5 +1,47 @@
 package cluedo.model;
 
 public class Card {
+    private final TYPE type;
+    private final String name;
 
+    public enum TYPE {
+        ROOM,
+        WEAPON,
+        CHARCTER,
+    }
+
+    public enum ROOM {
+        KITCHEN,
+        BALL_ROOM,
+        CONSERVATORY,
+        DINING_ROOM,
+        BILLIARD_ROOM,
+        LIBRARY,
+        LOUNGE,
+        HALL,
+        STUDY,
+    }
+
+    public enum WEAPON {
+        CANDLESTICK,
+        DAGGER,
+        LEAD_PIPE,
+        REVOLVER,
+        ROPE,
+        SPANNER,
+    }
+
+    public enum CHARACTER {
+        SCARLETT,
+        MUSTARD,
+        WHITE,
+        GREEN,
+        PEACOCK,
+        PLUM,
+    }
+
+    public Card(TYPE type, String name) {
+        this.type = type;
+        this.name = name;
+    }
 }
