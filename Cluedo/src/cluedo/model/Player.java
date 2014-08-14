a5c718c5fdc30d73e18d65d3d9d1b44a9e9776a7
kudos3939 <annie1020k@gmail.com>
2014-08-14 19:12:46 +1200

    added basic constructor(uid, token)

--- Cluedo/src/cluedo/model/Player.java
+++ Cluedo/src/cluedo/model/Player.java
@@ -1,5 +1,28 @@
 package cluedo.model;
 
+import java.util.ArrayList;
+
 public class Player {
+    public enum STATUS {
+        WATCHING,
+        MOVING,
+        MAKINGANNOUNCEMENT,
+        ROLLING,
+        REFUTING,
+        ELIMINATED,
+    }
+
+    private STATUS status;
+    private final long uid;
+    private ArrayList<Card> cards;
+    private final Chara character;
+    private int dice;
+
+    public Player(long uid, Chara character) {
+        this.uid = uid;
+        this.character = character;
+
+    }
+
 
 }
