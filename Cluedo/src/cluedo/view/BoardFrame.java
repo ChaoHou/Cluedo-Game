c3b49222ac8d23585b8c1ac882a24b0ccb294a78
kudos3939 <annie1020k@gmail.com>
2014-08-14 16:35:02 +1200

    added required methods

--- Cluedo/src/cluedo/view/BoardFrame.java
+++ Cluedo/src/cluedo/view/BoardFrame.java
@@ -2,24 +2,33 @@ package cluedo.view;
 
 import cluedo.model.*;
 
-import javax.swing.JFrame;
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseListener;
 
 public class BoardFrame extends JFrame {
     private final BoardCanvas canvas;
+    private boolean rollDisabled;
+    private boolean actionDisabled;
+    private boolean refuteDisabled;
 
-    public BoardFrame(String title, Board game, MouseListener mouse) {
+    public BoardFrame(String title, Board game, MouseListener... mouse) {
         super(title);
 
         canvas = new BoardCanvas(/* uid,*/game);
-        //it needs just one Listener?
-        canvas.addMouseListener(mouse);
+        //Master doesn't have listener
+        for (MouseListener m: mouse) {
+            canvas.addMouseListener(m);
+        }
 
         //does this line necessary?
         setLayout(new BorderLayout());
+
         add(canvas, BorderLayout.CENTER);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //create panel for buttons
+//        JPanel p = new JPanel();
 
         //pack components nicely
         pack();
@@ -28,6 +37,48 @@ public class BoardFrame extends JFrame {
 
         //set UI visible
         setVisible(true);
+    }
+
+    /**
+     *  disable the roll button
+     */
+    public void disableRoll() {
+
+    }
+
+    public void enableRoll() {
+
+    }
+
+    /**
+     * disabled the ability to make announcement or accusation. For example disable buttons.
+     */
+    public void disableAction() {
+
+    }
+
+    public void enableAction() {
+
+    }
+
+    /**
+     * disable the ability to select a card to refute.
+     *
+     */
+    public void disableRefute() {
+
+    }
+
+    public void enableRefute() {
+
+    }
+
+    public void initPlayer() {
+
+    }
+
+    public void showMessage(String str) {
+
     }
 
     public void repaint() {
