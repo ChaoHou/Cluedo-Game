0206b9afcd4bcb471e341114ee445580617edf25
ChaoHou <hch928ls@gmail.com>
2014-08-15 17:27:23 +1200

    Updated program process control part

--- Cluedo/src/cluedo/controller/action/client/SlaveAction.java
+++ Cluedo/src/cluedo/controller/action/client/SlaveAction.java
@@ -1,7 +1,8 @@
 package cluedo.controller.action.client;
 
 import cluedo.controller.action.Action;
+import cluedo.model.Board;
 
 public interface SlaveAction extends Action {
-	public void execute();
+	public void execute(Board game);
 }
